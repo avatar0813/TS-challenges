7237b645b5a9012952a43607beade56081a9475b
avatar <oooooooyk0813@gmail.com>
2023-04-12 22:06:39 +0800

    opt: pick 回顾总结

--- questions/easy-pick/template.ts
+++ questions/easy-pick/template.ts
@@ -3,3 +3,6 @@ type MyPick<T, K extends keyof T> = {
 }
 
 // https://www.typescriptlang.org/docs/handbook/2/keyof-types.html
+
+// 添加 K 的类型约束： K的类型(可能是union类型)必须extends T 的lookup 也就是说必须是 T的(keys)子集
+// 然后就可以直接使用 in 遍历union类型
