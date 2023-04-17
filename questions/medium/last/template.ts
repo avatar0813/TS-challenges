ee999f083122b23fc0a7fbae527676b3df3dd3bc
avatar <oooooooyk0813@gmail.com>
2023-04-17 21:15:45 +0800

    feat: medium Last 高级方法

--- questions/medium/last/template.ts
+++ questions/medium/last/template.ts
@@ -1,4 +1,6 @@
-type Last<T extends any[]> = T extends [...infer Rest, infer L] ? L : never
+// 普通方法
+// type Last<T extends any[]> = T extends [...infer Rest, infer L] ? L : never
 
 
-// easy game！
+// 高级方法
+type Last<T extends any[]> = [any, ...T][T['length']]
