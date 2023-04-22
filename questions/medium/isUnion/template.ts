d79b497719debfe21be1a99bab5e7840fd0e68e1
avatar <oooooooyk0813@gmail.com>
2023-04-22 16:21:59 +0800

    opt: 补充isUnion知识点

--- questions/medium/isUnion/template.ts
+++ questions/medium/isUnion/template.ts
@@ -12,6 +12,11 @@ type IsUnion<T, B = T> = [T] extends [never]
  * - 然后 union 类型开始使用 extends 判断
  *   T extends B 之后 的 [B] extends [T] 中的 第二个 [T] 其实已经是分发后的原union类型的单个元素类型
  *   所以 如果说 [B] extends [T] 还是成立的， 说明原本 T 就是一个单一类型 非union类型
+ * 
+ * 知识点
+ * union类型 extends 会分发
+ * union类型 会取大范围类型
+ * 判断是否为union类型 就是将类型分发之后再判断单一元素类型与原类型是否一样
  */
 
 type isUnionTest1 = 'a' | 1 | {age: 1}
