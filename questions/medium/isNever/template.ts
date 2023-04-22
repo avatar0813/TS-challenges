c3c9aacd585c46fe7107f7ebad5ae71bf6cac838
avatar <oooooooyk0813@gmail.com>
2023-04-22 15:44:48 +0800

    QS: never extends never 在类型编程与直接判断的不同表现

--- questions/medium/isNever/template.ts
+++ questions/medium/isNever/template.ts
@@ -1,3 +1,10 @@
 type IsNever<T> = [T] extends [never] ? true : false
 
 // 不能直接判断一个类型是never  但是可以使用[T] extends [never] 可以判断
+
+// 假如 isNever 是写成 T extends never
+// 为什么类型 aa 是 never
+// 而类型 bb 是 true
+type IsNever2<T> = T extends never ? true : false
+type aa = IsNever2<never> // never
+type bb = never extends never ? true : false // true
