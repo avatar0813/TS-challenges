6ef38a1ace0a7d498d32e76994e51cba802417f9
avatar <oooooooyk0813@gmail.com>
2023-09-19 11:15:26 +0800

    docs: 理解T extends never的使用场景

--- README.md
+++ README.md
@@ -6,8 +6,10 @@
 
 ## 待解决问题
 
-- `medium-deep-readonly` 中的 `T extends never` 是什么原理
-- `medium-chainable` 中的第三个case为什么没有通过
+已解决的查看对应题目中的`template.ts`
+
+- `medium-deep-readonly` 中的 `T extends never` 是什么原理？ `done`
+- `medium-chainable` 中的第三个case为什么没有通过？
 - `Permutation` 中的 联合类型 extends [never] 还没理解
 - `medium-index-signature` 中 `PropertyKey` 是什么
 - `medium-percentage-parser` 中 `${CheckPrefix<infer L>}`  怎么理解
--- questions/medium/deep-readonly/template.ts
+++ questions/medium/deep-readonly/template.ts
@@ -1,9 +1,21 @@
-type DeepReadonly<T> = { 
-  readonly [P in keyof T]: keyof T[P] extends never 
-  ? T[P] 
-  : DeepReadonly<T[P]> 
+type DeepReadonly<T> = {
+  readonly [P in keyof T]: keyof T[P] extends never
+  ? T[P]
+  : DeepReadonly<T[P]>
 }
-// 不太懂这里 keyof T[p] extends never
+/**
+ * keyof T[P] extends never
+ * 表示keyof T[P] 不能继续被拆分， never表示不会存在的类型
+ * keyof T[P] extends never ? true : false 为 true 时，也就表示T[P]是 undefined | null
+ */
+type testNever = never extends never ? true : false // true
+type testn = keyof (undefined | null) extends never ? true : false // true
 
+/**
+ * keyof string
+ * 会返回带有 number 以及string原型对象上所有属性的联合类型
+ * 带有number是对象类型中有类数组对象的场景
+ */
+type keyStr = keyof string // number | "toString" | "charAt"  | ...
 
-type test = (keyof string) extends never ? true : false // false ?
+type testStr = keyof string extends string | number ? true : false // true  包含number的类数组场景
