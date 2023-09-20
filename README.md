503ff2e3a87ee81352cb5f4c0e7665030f5305f7
avatar <oooooooyk0813@gmail.com>
2023-09-21 00:03:57 +0800

    doc: 理解infer L 做泛型参数

--- README.md
+++ README.md
@@ -12,5 +12,5 @@
 - `medium-chainable` 中的第三个case为什么没有通过？
 - `Permutation` 中的 联合类型 extends [never] 还没理解
 - `medium-index-signature` 中 `PropertyKey` 是什么? `done`
-- `medium-percentage-parser` 中 `${CheckPrefix<infer L>}`  怎么理解
+- `medium-percentage-parser` 中 `${CheckPrefix<infer L>}`  怎么理解? `done`
 - `medium-FlattenDepth`  <https://github.com/type-challenges/type-challenges/tree/main/questions/03243-medium-flattendepth>
--- questions/medium/percentage-parser/template.ts
+++ questions/medium/percentage-parser/template.ts
@@ -1,10 +1,10 @@
-type CheckPrefix<T> = T extends '+' | '-' ? T : never;
-type CheckSuffix<T> =  T extends `${infer P}%` ? [P, '%'] : [T, ''];
+type CheckPrefix<T> = T extends '+' | '-' ? T : never
+type CheckSuffix<T> = T extends `${infer P}%` ? [P, '%'] : [T, '']
 
-type PercentageParser<A extends string> = 
-A extends `${CheckPrefix<infer L>}${infer R}` 
-? [L, ...CheckSuffix<R>] 
-: ['', ...CheckSuffix<A>];
+type PercentageParser<A extends string> =
+  A extends `${CheckPrefix<infer L>}${infer R}`
+  ? [L, ...CheckSuffix<R>]
+  : ['', ...CheckSuffix<A>];
 
 /**
  * 解题思路：
@@ -14,4 +14,6 @@ A extends `${CheckPrefix<infer L>}${infer R}`
  * 
  * 核心
  * 怎么理解 CheckPrefix<infer L> 中是将L传进去，还是说是CheckPrefix高级类型返回的L
+ * 
+ * 表示 从 A中 拆分的L 能满足 checkPrefix的类型推断，可以直接将L套入checkPrefix的表达式
  */
