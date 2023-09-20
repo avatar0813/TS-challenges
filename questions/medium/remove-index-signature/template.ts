d5d66e2b9bb93e01de2cfd1d52c141ad0c97812b
avatar <oooooooyk0813@gmail.com>
2023-09-20 23:50:18 +0800

    docs: PropertyKey的含义

--- questions/medium/remove-index-signature/template.ts
+++ questions/medium/remove-index-signature/template.ts
@@ -1,15 +1,18 @@
 type RemoveIndexSignature<T> = {
   [
-    K in keyof T 
-    as (
-      PropertyKey extends keyof T[K] 
-        ? never 
-        : K
-      )
-  ]: T[K];
+  K in keyof T
+  as (
+    PropertyKey extends keyof T[K]
+    ? never
+    : K
+  )
+  ]: T[K]
 };
 
 /**
  * 问题点：怎么判断是 标记类型索引
  * PropertyKey 是什么？
+ * 
+ * declare type PropertyKey = string | number | symbol;
+ * 内置类型： 表示对象的所有可能的属性名类型
  */
