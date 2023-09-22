7c9444d54b28ad0f64455a3380c4de2938fe45b4
avatar <oooooooyk0813@gmail.com>
2023-09-23 00:01:00 +0800

    feat: chainable 链式调用

--- questions/medium/chainable/template.ts
+++ questions/medium/chainable/template.ts
@@ -1,11 +1,14 @@
-type Chainable<T extends Record<string, any> = {}> = {
-  option<K extends string, V>(key: K extends keyof T ? never : K, value: V): Chainable<T & Record<K, V>> 
-  get(): T
+
+type Chainable<T = {}> = {
+  option: <K extends string, V>(key: K extends keyof T ?
+    V extends T[K] ? never : K
+    : K, value: V) => Chainable<Omit<T, K> & Record<K, V>>
+  get: () => T
 }
 
-
 /**
- * 属性类型为函数, 先<指定泛型类型>
- * 取K：在参数中指定参数类型限制( K extends keyof T ? never : K )
- * 赋值: 对象赋值的过程， 即为 {} & {K, V} 递归传递给Chainable中的T
+ * 链式调用， option 是添加属性的过程，get 是返回实例
+ * 
+ * 所以option中如果已存在的类型，则不添加，只添加未存在的 ==> V extends T[K] ? never : K
+ * option 返回的是重组的对象(包含原对象与新添加的对象), 所以为===> Omit<T, K> & Record<K, V>，
  */
